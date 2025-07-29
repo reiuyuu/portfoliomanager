707ea56863c2884bd80e24c2917794e2f147c544
Trista <trista_23@163.com>
2025-07-29 13:41:25 +0800

    test stocks function

---
+++ server/test-stocks.ts
@@ -0,0 +1,84 @@
+// import { db } from './src/config/db'
+
+// async function getStocks() {
+//   const { data, error } = await db.from('stocks').select('*').limit(5)
+
+//   if (error) {
+//     console.error('Error fetching stocks:', error)
+//     return
+//   }
+
+//   console.log('Stocks data:', data)
+// }
+
+// // Run the function
+// getStocks()
+
+import axios from 'axios'
+
+import { db } from './src/config/db'
+
+const API_KEY = process.env.STOCKS_API_KEY
+const BASE_URL = 'https://api.twelvedata.com/price'
+
+interface StockInfo {
+  id: number
+  symbol: string
+}
+
+// get specific stock current price
+async function getCurrentPrice(symbol: string): Promise<number | null> {
+  try {
+    const res = await axios.get(
+      `${BASE_URL}?symbol=${symbol}&apikey=${API_KEY}`,
+    )
+    const price = res.data?.price
+    if (!price) return null
+    return Number(price)
+  } catch (err) {
+    console.error(`Get ${symbol} price failed:`, err)
+    return null
+  }
+}
+
+async function updateStockPrices() {
+  const { data: stockData, error: stockErr } = await db
+    .from('stocks')
+    .select('id,symbol')
+
+  if (stockErr) {
+    console.error('Error fetching stocks:', stockErr)
+    return
+  }
+
+  const stocksList: StockInfo[] = stockData || []
+  console.log('Fetched stocks:', stocksList)
+
+  for (const stock of stocksList) {
+    const price = await getCurrentPrice(stock.symbol)
+    if (price === null) continue
+
+    const today = new Date().toISOString().slice(0, 10)
+
+    const { error: insertErr } = await db.from('stock_prices').insert([
+      {
+        stock_id: stock.id,
+        price,
+        date: today,
+        created_at: new Date(),
+      },
+    ])
+
+    if (insertErr) {
+      console.error(`Insert ${stock.symbol} price failed:`, insertErr)
+    } else {
+      console.log(
+        `✅ Inserted ${stock.symbol} (stock_id=${stock.id}) price $${price}`,
+      )
+    }
+  }
+
+  console.log("📈 All stocks' current price updated!")
+}
+
+updateStockPrices()
